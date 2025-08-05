22a8bff4a88efa3f14a39ced8e2dc16742f0275f
Ankit-Dalei <ankitdalei123@gmail.com>
2025-08-05 22:52:49 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     port: parseInt(process.env.PORT) || 4040,
+    host: '0.0.0.0'
   },
   plugins: [
     react(),
